17df2e44ac587eaad228907679adcaeb6e1320b8
Mateus Sobrinho <mateus.webphp@gmail.com>
2022-07-27 13:31:53 -0300

    maping bits from websocket

--- nodejs-raw-websocket/server.mjs
+++ nodejs-raw-websocket/server.mjs
@@ -4,6 +4,12 @@ import crypto from 'crypto'
 
 const PORT = 1337
 const WEBSOCKET_MAGIC_STRING_KEY = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+const SEVEN_BITS_INTEGER_MARKER = 125
+const SIXTEEN_BITS_INTEGER_MARKER = 126
+const SIXTYFOUR_BITS_INTEGER_MARKER = 127
+const MASK_KEY_BYTES_LENGTH = 4
+
+const FIRST_BIT = 128
 
 const server = createServer((request, response) => {
     response.writeHead(200)
@@ -13,12 +19,31 @@ const server = createServer((request, response) => {
 
   server.on('upgrade', onSocketUpgrade)
 
+function onSocketReadable(socket) {
+  socket.read(1)
+
+  const [markerAndPayloadLenght] = socket.read(1)
+  const lengthIndicatorInBits = markerAndPayloadLenght - FIRST_BIT
+
+  let messageLength = 0
+  if( lengthIndicatorInBits <= SEVEN_BITS_INTEGER_MARKER) {
+    messageLength = lengthIndicatorInBits
+  } else {
+    throw new Error(`your message is too long! we dont handle 64-bits messages`)
+  }
+
+  const maskKey = socket.read(MASK_KEY_BYTES_LENGTH)
+  const encoded = socket.read(messageLength)
+  console.log()
+}
+
 function onSocketUpgrade(req, socket, hand){
   const { 'sec-websocket-key': webClientSocketKey } = req.headers
   console.log(`${webClientSocketKey} connected!`)
   const headers = prepareHandShakeHeaders(webClientSocketKey)
 
   socket.write(headers);
+  socket.on('readable', () => onSocketReadable(socket))
 }
 
 function prepareHandShakeHeaders(id) {
