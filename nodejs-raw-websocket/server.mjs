b4658c24517daee1fd1f1b49d3141332106bb35f
Mateus Sobrinho <mateus.webphp@gmail.com>
2022-07-27 12:26:42 -0300

    websocket connection established

--- nodejs-raw-websocket/server.mjs
+++ nodejs-raw-websocket/server.mjs
@@ -1,13 +1,43 @@
 import { createServer } from 'node:http'
-const PORT = 1337
+import crypto from 'crypto'
 
-createServer((request, response) => {
+
+const PORT = 1337
+const WEBSOCKET_MAGIC_STRING_KEY = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+
+const server = createServer((request, response) => {
     response.writeHead(200)
-    throw new Error('teste')
     response.end('hey there')
   })
   .listen(1337, () => console.log('server is listening to', PORT))
 
+  server.on('upgrade', onSocketUpgrade)
+
+function onSocketUpgrade(req, socket, hand){
+  const { 'sec-websocket-key': webClientSocketKey } = req.headers
+  console.log(`${webClientSocketKey} connected!`)
+  const headers = prepareHandShakeHeaders(webClientSocketKey)
+
+  socket.write(headers);
+}
+
+function prepareHandShakeHeaders(id) {
+  const acceptKey = createSocketAccecpt(id)
+  const headers = [
+    'HTTP/1.1 101 Switching Protocols',
+    'Upgrade: websocket',
+    'Connection: Upgrade',
+    `Sec-WebSocket-Accept:${acceptKey}`,
+    ''
+  ].map(line => line.concat('\r\n')).join('')
+  return headers
+}
+
+function createSocketAccecpt(id) {
+  const shaum = crypto.createHash('sha1')
+  shaum.update(id + WEBSOCKET_MAGIC_STRING_KEY)
+  return shaum.digest('base64')
+}
 ;
 [
   "uncaughtException",
